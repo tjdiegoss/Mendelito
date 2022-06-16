513c3b5adb3bfdaf8a26dfe6c64f08e8ffad23af
Diego <Diego@Enigma>
2022-06-16 09:15:03 -0300

    Primeiro Commit

---
+++ Mendelius/src/main/java/br/com/tjdiego/mendelius/Mendelius.java
@@ -0,0 +1,13 @@
+
+package br.com.tjdiego.mendelius;
+
+/**
+ *
+ * @author Diego
+ */
+public class Mendelius {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Mendelius/src/main/java/br/com/tjdiego/mendelius/Teste.java
@@ -0,0 +1,10 @@
+
+package br.com.tjdiego.mendelius;
+
+/**
+ *
+ * @author Diego
+ */
+public class Teste {
+    
+}
