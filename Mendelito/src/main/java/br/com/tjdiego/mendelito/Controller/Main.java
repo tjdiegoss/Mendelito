e69530e6a63fe169399faaa49c9944c8a68eabfd
Diego <Diego@Enigma>
2022-06-22 10:50:03 -0300

    Começando a usar lógica aqui

--- Mendelito/src/main/java/br/com/tjdiego/mendelito/Controller/Main.java
+++ Mendelito/src/main/java/br/com/tjdiego/mendelito/Controller/Main.java
@@ -7,4 +7,9 @@ package br.com.tjdiego.mendelito.Controller;
  */
 public class Main {
     
+    public void main (String args []){
+    
+        System.out.println("teste");
+        
+    }
 }
--- Mendelito/src/main/java/br/com/tjdiego/mendelito/Teste.java
+++ Mendelito/src/main/java/br/com/tjdiego/mendelito/Teste.java
@@ -11,8 +11,16 @@ public class Teste {
     
     public static void main (String [] args){
     
+         boolean gene1, gene2;
+         gene1 = true;
+         gene2 = true;
+         
+         if (gene1 == false ){
+             JOptionPane.showInputDialog("Dominate");
+         }else
+             JOptionPane.showInputDialog("Recessivo");
         //System.out.println("Teste");
-        JOptionPane.showInputDialog("Teste");
+        
     }
     
 }
